d975438ff93b9961e9b9193cd0bb6fd97fe6172b
John King <j.king.sq@gmail.com>
2018-03-05 21:03:38 -0500

    moved implementations out of Ray module

--- raytracer/Ray.c
+++
@@ -1,7 +0,0 @@
-#include "Ray.h"
-
-SDL_Color trace(Ray ray) {
-    ray.direction = vectorNormalize(ray.direction);
-    SDL_Color result = {ray.direction.x, ray.direction.y, ray.direction.z, 255};
-    return result;
-}
